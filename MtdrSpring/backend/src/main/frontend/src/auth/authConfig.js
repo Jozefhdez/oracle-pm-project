f02301bf72781be4a2275bbe6ab73eb4e1969015
Jozef David Hernández Campos <jozefhdez2@gmail.com>
2026-05-13 17:58:00 -0600

    fix: hardcode OIDC config in authConfig.js

--- MtdrSpring/backend/src/main/frontend/src/auth/authConfig.js
+++ MtdrSpring/backend/src/main/frontend/src/auth/authConfig.js
@@ -1,7 +1,7 @@
 export const oidcConfig = {
-  authority: process.env.REACT_APP_OIDC_AUTHORITY,
-  client_id: process.env.REACT_APP_OIDC_CLIENT_ID,
-  client_secret: process.env.REACT_APP_OIDC_CLIENT_SECRET,
+  authority: 'https://idcs-a333fea8b68e4aff8867ff6094453a03.identity.oraclecloud.com',
+  client_id: '7809ed300a374eafa7bb9403f8f1ff01',
+  client_secret: 'd1ca0425-b462-45b6-ac8f-3f4445c19f09',
   client_authentication: 'client_secret_post',
   redirect_uri: `${window.location.origin}/callback`,
   scope: 'openid profile email',
