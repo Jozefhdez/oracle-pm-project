cd47db8f471d8cf5b44dde800845b6ec8471e669
Jozef David Hernández Campos <jozefhdez2@gmail.com>
2026-05-13 18:25:12 -0600

    ui: update dashboard subtitle text

--- MtdrSpring/backend/src/main/frontend/src/views/dashboard/DashboardView.jsx
+++ MtdrSpring/backend/src/main/frontend/src/views/dashboard/DashboardView.jsx
@@ -214,7 +214,7 @@ export default function DashboardView({
           Welcome back, {userName}
         </Typography>
         <Typography sx={{ fontSize: '0.875rem', color: '#717171' }}>
-          Here is what is happening with {activeSprintName} today.
+          Here is your latest update for {activeSprintName}.
         </Typography>
       </Box>
 
